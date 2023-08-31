cc1bfeae20421b263a1690fa89029776004215c9
Bernhard Schmitt <schmitt@sitegeist.de>
2023-08-31 15:47:24 +0200

    WIP: use ImageAnotator client

--- Classes/Domain/GoogleCloudVisionInterpreter.php
+++ Classes/Domain/GoogleCloudVisionInterpreter.php
@@ -8,6 +8,8 @@ declare(strict_types=1);
 
 namespace Sitegeist\ArtClasses\GoogleCloudVisionInterpreter\Domain;
 
+use Google\Cloud\Vision\V1\Feature\Type as FeatureType;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 use Neos\Flow\I18n\Locale;
 use Neos\Media\Domain\Model\Image;
 use Sitegeist\ArtClasses\Domain\Interpretation\ImageInterpreterInterface;
@@ -25,5 +27,27 @@ final class GoogleCloudVisionInterpreter implements ImageInterpreterInterface
 
     public function interpretImage(Image $image, ?Locale $targetLocale): ImageInterpretation
     {
+        $annotator = new ImageAnnotatorClient([
+            'credentials' => $this->credentialsFilePath
+        ]);
+        $annotation = $annotator->annotateImage(
+            $image->getResource()->getStream(),
+            [
+                FeatureType::FACE_DETECTION,
+                FeatureType::LANDMARK_DETECTION,
+                FeatureType::LOGO_DETECTION,
+                FeatureType::LABEL_DETECTION,
+                FeatureType::TEXT_DETECTION,
+                FeatureType::DOCUMENT_TEXT_DETECTION,
+                FeatureType::SAFE_SEARCH_DETECTION,
+                FeatureType::IMAGE_PROPERTIES,
+                FeatureType::CROP_HINTS,
+                FeatureType::WEB_DETECTION,
+                FeatureType::PRODUCT_SEARCH,
+                FeatureType::OBJECT_LOCALIZATION,
+            ]
+        );
+
+        return new ImageInterpretation(null, null);
     }
 }
